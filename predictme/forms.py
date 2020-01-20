bda1484402011fc5485cf271716288e2fbbe2e1b
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2020-01-20 12:07:17 +0100

    Update form

--- predictme/forms.py
+++ predictme/forms.py
@@ -7,4 +7,8 @@ from django import forms
 class UploadFileForm(forms.Form):
     file = forms.FileField()
 
-
+    def __init__(self, *args, **kwargs):
+        super(UploadFileForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'box__file'
+            visible.field.widget.attrs['id'] = 'file'
