8d675e04645f065c6d71ac18e9961184f4cb1642
daniel.domingo.fernandez <danieldomingofernandez@hotmail.com>
2020-01-25 15:46:45 +0100

    Add install R libraries

--- R/install_R_libraries.R
+++ R/install_R_libraries.R
@@ -1,3 +1,7 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(version = "3.10")
+
 # Install packages required to run PredictMe
 install.packages("h2o")
 install.packages("randomForest")
